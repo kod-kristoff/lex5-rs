1a2ffd4de81367b5c01a4574d44f9de2eb0f6047
Kristoffer Andersson <kod.kristoff@gmail.com>
2025-09-13 13:07:20 +0200

    add HttpServer stub

---
+++ lex-http/src/server.rs
@@ -0,0 +1,56 @@
+/// Configuration for the HTTP server.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HttpServerConfig<'a> {
+    pub port: &'a str,
+}
+
+/// The application's HTTP server. The underlying HTTP package is opaque to module consumers.
+pub struct HttpServer {
+    // router: axum::Router,
+    // listener: net::TcpListener,
+}
+
+impl HttpServer {
+    /// Returns a new HTTP server bound to the port specified in `config`.
+    pub async fn new(
+        // blog_service: impl BlogService,
+        config: HttpServerConfig<'_>,
+    ) -> Result<Self, String> {
+        println!("Hello, world!");
+        // let trace_layer = tower_http::trace::TraceLayer::new_for_http().make_span_with(
+        //     |request: &axum::extract::Request<_>| {
+        //         let uri = request.uri().to_string();
+        //         tracing::info_span!("http_request", method = ?request.method(), uri)
+        //     },
+        // );
+
+        // Construct dependencies to inject into handlers.
+        // let state = AppState {
+        //     author_service: Arc::new(blog_service),
+        // };
+        //
+        // let router = axum::Router::new()
+        //     .nest("/api", api_routes())
+        //     .layer(trace_layer)
+        //     .with_state(state);
+
+        // let listener = net::TcpListener::bind(format!("0.0.0.0:{}", config.port))
+        //     .await
+        //     .with_context(|| format!("failed to listen on {}", config.port))?;
+
+        Ok(Self {
+            // router, 
+            // listener 
+        })
+    }
+
+    /// Runs the HTTP server.
+    pub async fn run(self) -> Result<(), String> {
+        println!("Hello, world!");
+        // tracing::debug!("listening on {}", self.listener.local_addr().unwrap());
+        // axum::serve(self.listener, self.router)
+        //     .await
+        //     .context("received error from running server")?;
+        Ok(())
+    }
+}
--- lex-server/src/main.rs
+++ lex-server/src/main.rs
@@ -1,4 +1,9 @@
+use lex_http::{HttpServer, HttpServerConfig};
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), String> {
+    let config = HttpServerConfig { port: "3000" };
+
+    let server = HttpServer::new(config).await.unwrap();
+    server.run().await
 }
